7e5b1dd435c66a15bd2613796e480034097dbe42
Advi729 <44499017+Advi729@users.noreply.github.com>
2022-12-19 01:03:44 +0530

    did connection of db

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,76 @@
 var express = require('express');
 var router = express.Router();
+var productHelper = require('../helpers/product-helpers');
+
+const product = [{
+  name: 'HP Victus 16',
+  price: 55000,
+  description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
+  image: 'images/laptop.jpg'
+},
+{
+  name: 'HP Victus 17',
+  price: 65000,
+  description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
+  image: 'images/laptop.jpg' 
+},
+{
+  name: 'HP Victus 18',
+  price: 58000,
+  description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
+  image: 'images/laptop.jpg'   
+},
+{
+  name: 'HP Victus 19',
+  price: 85000,
+  description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
+  image: 'images/laptop.jpg'   
+},
+{
+  name: 'HP Victus 20',
+  price: 50000,
+  description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
+  image: 'images/laptop.jpg'
+},
+{
+  name: 'HP Victus 21',
+  price: 78000,
+  description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
+  image: 'images/laptop.jpg' 
+},
+{
+  name: 'HP Victus 22',
+  price: 46000,
+  description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
+  image: 'images/laptop.jpg'   
+},
+{
+  name: 'HP Victus 23',
+  price: 80000,
+  description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
+  image: 'images/laptop.jpg'   
+}
+]
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/view-products',{admin:true});
+  res.render('admin/view-products',{product,admin:true});
 });
+
+router.get('/add-product', function(req, res) {
+  res.render('admin/add-product');
+});
+
+router.post('/add-product', (req, res) => {
+  console.log(req.body);
+  console.log(req.files.Image);
+
+  productHelper.addProduct(req.body,(result)=>{
+    res.render('admin/add-product');
+  });
+})
+
+
+
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,45 +6,53 @@ router.use(express.static(path.join(__dirname, 'public')));
 
 const product = [{
     name: 'HP Victus 16',
+    price: 55000,
     description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
     image: 'images/laptop.jpg'
-},
-{
+  },
+  {
     name: 'HP Victus 17',
+    price: 65000,
     description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
     image: 'images/laptop.jpg' 
-},
-{
+  },
+  {
     name: 'HP Victus 18',
+    price: 58000,
     description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
     image: 'images/laptop.jpg'   
-},
-{
+  },
+  {
     name: 'HP Victus 19',
+    price: 85000,
     description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
     image: 'images/laptop.jpg'   
-},
-{
+  },
+  {
     name: 'HP Victus 20',
+    price: 50000,
     description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
     image: 'images/laptop.jpg'
-},
-{
+  },
+  {
     name: 'HP Victus 21',
+    price: 78000,
     description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
     image: 'images/laptop.jpg' 
-},
-{
+  },
+  {
     name: 'HP Victus 22',
+    price: 46000,
     description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
     image: 'images/laptop.jpg'   
-},
-{
+  },
+  {
     name: 'HP Victus 23',
+    price: 80000,
     description: 'HP Victus 16-e0550AX Gaming Laptop (AMD Ryzen 7 5800H/ 8GB/ 512GB SSD/ Win11/ 4GB Graph)',
     image: 'images/laptop.jpg'   
-}
-]
+  }
+  ]
 
 // router.get('/', (req, res) => {
 //     if(req.session.user) {   
